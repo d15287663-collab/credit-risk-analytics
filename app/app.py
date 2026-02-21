e226dcbbc56c88fe6a6eccc070032c77ca773a65
d15287663-collab <d15287663@gmail.com>
2026-02-21 12:33:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,16 +2,20 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-st.title("Credit Risk Dashboard")
+st.title("Credit Risk PD Model")
 
-model = joblib.load("../models/pd_model.pkl")
-scaler = joblib.load("../models/scaler.pkl")
+# load model
+model = joblib.load("pd_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.write("Enter customer details")
 
 age = st.slider("Age", 18, 75, 30)
 credit = st.number_input("Credit Amount", 500, 20000, 5000)
 duration = st.slider("Duration", 6, 72, 24)
 
-if st.button("Predict Risk"):
+if st.button("Predict Default Risk"):
+
     df = pd.DataFrame({
         "Age":[age],
         "Credit amount":[credit],
@@ -21,4 +25,4 @@ if st.button("Predict Risk"):
     df_scaled = scaler.transform(df)
     prob = model.predict_proba(df_scaled)[0][1]
 
-    st.write(f"Default Probability: {prob:.2f}")
+    st.success(f"Default Probability: {prob:.2f}")
